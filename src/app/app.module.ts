14040ecd61138a6aab694e87632a13f814b1158b
regi2715 <emilianomacias0@gmail.com>
2019-05-20 01:11:35 -0500

    Se creo home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from './components/home.component';
 
 @NgModule({
-  declarations: [
+  declarations:[
+    HomeComponent,
     AppComponent
   ],
   imports: [
---
+++ src/app/components/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'home',
+    templateUrl:'../views/home.html'
+})
+export class HomeComponent {
+    public titulo: string;
+    constructor(){
+        this.titulo = 'WebApp de productos con Angular 4';
+    }
+
+    ngOnInit(){
+        console.log('Se ah cargado el componente home.ts');
+    }
+  }
